74d7020d5f203c0531776d6214ddab97f0dfd03e
frackz <65610875+frackz@users.noreply.github.com>
2023-10-06 08:06:19 +0200

    Comments

--- server/resources/spark/server/Version.lua
+++ server/resources/spark/server/Version.lua
@@ -2,10 +2,14 @@ Spark.Version = {
     URL = "https://pastebin.com/raw/JuV2wzZ6"
 }
 
+--- Get the current version Spark is running on.
+--- @return number?
 function Spark.Version:Get()
     return tonumber(GetResourceMetadata(GetCurrentResourceName(), 'version', 0))
 end
 
+--- Get the newest version of Spark.
+--- @return number | nil
 function Spark.Version:Newest()
     local text, status = Spark.Network:HTTP(self.URL, 'GET')
     if status == 200 then
