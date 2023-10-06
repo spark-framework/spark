d31b0073d807d6af3653b92c9eeae7675aac6673
frackz <65610875+frackz@users.noreply.github.com>
2023-10-06 09:13:46 +0200

    table module

---
+++ server/resources/spark/shared/Table.lua
@@ -0,0 +1,16 @@
+Spark.Table = {}
+
+--- Check if a table contains a value
+--- @param table table
+--- @param value any
+--- @return boolean
+function Spark.Table:Contains(table, value)
+    for _, v in pairs(table) do
+        if v == value then
+            return true
+        end
+    end
+
+    return false
+end
+
